e5bde8b51122bd54c77ca470b00d72a4bf516b8a
Pritam Shinde <44087724+pritam-shinde@users.noreply.github.com>
2025-01-06 16:28:36 +0530

    added heading in typography and offer images optimise

--- sections/Home/PaymentPlan/PaymentPlan.js
+++ sections/Home/PaymentPlan/PaymentPlan.js
@@ -1,4 +1,4 @@
-import { Box, Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { Autoplay } from "swiper";
@@ -22,75 +22,83 @@ import smile from "../../../public/Home/PaymentPlan/smile.jpg";
 import WESTFUND from "../../../public/Home/PaymentPlan/WESTFUND.jpg";
 
 const PaymentPlan = () => {
-    const [width, setWidth] = useState();
+  const [width, setWidth] = useState();
 
-    useEffect(() => {
-        setWidth(window.innerWidth);
-    }, [width]);
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, [width]);
 
-    return (
-        <>
-            <Container
-                maxWidth="xxl"
-                sx={{
-                    margin: { xs: "40px 0px 40px 0px", md: "60px 0px 60px 0px" },
-                }}
-            >
-                <Grid container>
-                    <Grid item xs={12} md={12} className="mx-auto">
-                        <Box py={2}>
-                            <SectionalHeading
+  return (
+    <>
+      <Container
+        maxWidth="xxl"
+        sx={{
+          margin: { xs: "40px 0px 40px 0px", md: "60px 0px 60px 0px" },
+        }}
+      >
+        <Grid container>
+          <Grid item xs={12} md={12} className="mx-auto">
+            <Box py={2}>
+              {/* <SectionalHeading
                                 variant="h2"
                                 align="center"
                                 color="var(--dark-blue)"
                                 title="Payment Options"
                                 style={{ fontSize: "clamp(40px,4vw,75px) !important" }}
-                            />
-                        </Box>
-                        <Box py={3}>
-                            <Swiper
-                                slidesPerView={
-                                    width > 992 ? 6 : width > 0 && width < 600 ? 2 : 3
-                                }
-                                spaceBetween={50}
-                                modules={[Autoplay]}
-                                className="mySwiper"
-                                autoplay={{
-                                    delay: 2500,
-                                    disableOnInteraction: false,
-                                }}
-                                loop={true}
-                            >
-                                {[
-                                    { image: AfterPay, alt: "afterpay" },
-                                    { image: Bupa, alt: "Bupa" },
-                                    { image: CBHS, alt: "CBHS" },
-                                    { image: Denticare, alt: "denticare" },
-                                    { image: EFTPOS, alt: "EFTPOS" },
-                                    { image: HFC, alt: "HCF" },
-                                    { image: Hicaps, alt: "Hicaps" },
-                                    { image: Medibank, alt: "Medibank" },
-                                    { image: Medicare, alt: "Medicare" },
-                                    { image: MySmilePlan, alt: "MySmilePlan" },
-                                    { image: smile, alt: "smile" },
-                                    { image: WESTFUND, alt: "WESTFUND" },
-                                ].map((item, index) => (
-                                    <SwiperSlide key={`Home_payment_plan_${item.alt}`}>
-                                        <Image
-                                            src={item.image}
-                                            alt={item.alt}
-                                            width={180}
-                                            height={80}
-                                        />
-                                    </SwiperSlide>
-                                ))}
-                            </Swiper>
-                        </Box>
-                    </Grid>
-                </Grid>
-            </Container>
-        </>
-    );
+                            /> */}
+              <Typography
+                component="div"
+                variant="h2"
+                className="text-dark-blue text-start text-md-center fw-semibold "
+                style={{ fontSize: "clamp(40px,5vw,75px) !important" }}
+              >
+                Payment Options
+              </Typography>
+            </Box>
+            <Box py={3}>
+              <Swiper
+                slidesPerView={
+                  width > 992 ? 6 : width > 0 && width < 600 ? 2 : 3
+                }
+                spaceBetween={50}
+                modules={[Autoplay]}
+                className="mySwiper"
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+                }}
+                loop={true}
+              >
+                {[
+                  { image: AfterPay, alt: "afterpay" },
+                  { image: Bupa, alt: "Bupa" },
+                  { image: CBHS, alt: "CBHS" },
+                  { image: Denticare, alt: "denticare" },
+                  { image: EFTPOS, alt: "EFTPOS" },
+                  { image: HFC, alt: "HCF" },
+                  { image: Hicaps, alt: "Hicaps" },
+                  { image: Medibank, alt: "Medibank" },
+                  { image: Medicare, alt: "Medicare" },
+                  { image: MySmilePlan, alt: "MySmilePlan" },
+                  { image: smile, alt: "smile" },
+                  { image: WESTFUND, alt: "WESTFUND" },
+                ].map((item, index) => (
+                  <SwiperSlide key={`Home_payment_plan_${item.alt}`}>
+                    <Image
+                      src={item.image}
+                      alt={item.alt}
+                      width={180}
+                      height={80}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 };
 
 export default PaymentPlan;
